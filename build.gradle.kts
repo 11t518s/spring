a40c71e6b7a71f45beaa8359f682d6056a5f8ca1
11t518s <11t518s20161759@gmail.com>
2024-10-03 13:57:20 +0900

    의존성 중 불필요한 부분 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	id("org.springframework.boot") version "2.7.18"
-	id("io.spring.dependency-management") version "1.1.6"
 	id("com.google.cloud.tools.jib") version "3.1.1"
 }
 
@@ -35,20 +34,18 @@ repositories {
 }
 
 dependencies {
+	implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
 	}
 }
 
