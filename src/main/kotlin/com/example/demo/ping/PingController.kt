d7909a6e13adb0606b1cb9789177108feb38c256
11t518s <11t518s20161759@gmail.com>
2024-10-03 20:48:50 +0900

    fix: ping pong api에서 환경변수 값을 가져오도록 수정

--- src/main/kotlin/com/example/demo/ping/PingController.kt
+++ src/main/kotlin/com/example/demo/ping/PingController.kt
@@ -1,12 +1,15 @@
 package com.example.demo.ping
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RequestMapping("/ping")
 @RestController
 class PingController {
-    @GetMapping("/ping")
-    fun pingPongTest(): String {
-        return "pong"
+    @GetMapping("")
+    fun pingPongTest(@Value("\${fixed.value.pong}") keyword: String ): String {
+        return keyword
     }
 }
